1de1cbbb67c6588848ec2712d1975f4c13771efb
grishasobol <grishasobol@mail.ru>
2015-10-02 18:28:18 +0300

    Update vkfinder.py

--- working scripts/vkfinder.py
+++ working scripts/vkfinder.py
@@ -4,7 +4,7 @@ import vkontakte
 import json
 import time
 
-(token,user_id) = vk_auth.auth('сюда введи мыло(свое)', 'сюда введи пароль(свой)', '5054584', "photos")
+(token,user_id) = vk_auth.auth('хуй', 'сюда введи пароль(свой)', '5054584', "photos")
 vk = vkontakte.API(token=token)
 print "Hello from VK API , server time is ",vk.getServerTime()
 
